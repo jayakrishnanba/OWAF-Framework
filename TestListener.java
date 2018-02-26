80a1925e2b709435b50577d03dbfe972368c130b
Jayakrishnan B <36568990+jayakrishnanba@users.noreply.github.com>
2018-02-26 08:08:32 +0530

    Add files via upload

---
+++ TestListener.java
@@ -0,0 +1,121 @@
+package framework.OWAF.utils;
+
+
+import io.qameta.allure.Attachment;
+
+import java.util.List;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+import org.testng.Reporter;
+
+import framework.OWAF.tests.AppTest;
+
+
+
+public class TestListener extends AppTest implements ITestListener {
+
+    private static String getTestMethodName(ITestResult iTestResult) {
+        return iTestResult.getMethod().getConstructorOrMethod().getName();
+    }
+
+    //Text attachments for Allure
+    @Attachment(value = "Page screenshot", type = "image/png")
+    public byte[] saveScreenshotPNG (WebDriver driver) {
+        return ((TakesScreenshot)driver).getScreenshotAs(OutputType.BYTES);
+    }
+
+    //Text attachments for Allure
+    @Attachment(value = "{0}", type = "text/plain")
+    public static String saveTextLog (String message) {
+        return message;
+    }
+
+    //HTML attachments for Allure
+    @Attachment(value = "{0}", type = "text/html")
+    public static String attachHtml(String html) {
+        return html;
+    }
+    
+    @Attachment
+    public String logOutput(List<String> outputList) {
+        String output = ""; 
+        for (String o : outputList) 
+            output += o + "\n"; 
+        return output;
+    }
+
+    @Override
+    public void onStart(ITestContext iTestContext) {
+        System.out.println("I am in onStart method " + iTestContext.getName());
+        iTestContext.setAttribute("WebDriver", this.driver);
+    }
+
+    @Override
+    public void onFinish(ITestContext iTestContext) {
+        System.out.println("I am in onFinish method " + iTestContext.getName());
+        //Do tier down operations for extentreports reporting!
+//        ExtentTestManager.endTest();
+//        ExtentManager.getReporter().flush();
+    }
+
+    @Override
+    public void onTestStart(ITestResult iTestResult) {
+        System.out.println("I am in onTestStart method " +  getTestMethodName(iTestResult) + " start");
+        //Start operation for extentreports.
+       // ExtentTestManager.startTest(iTestResult.getMethod().getMethodName(),"");
+    }
+
+    @Override
+    public void onTestFailure(ITestResult iTestResult) {
+        System.out.println("I am in onTestFailure method " +  getTestMethodName(iTestResult) + " Failure");
+        //Extentreports log operation for passed tests.
+     //   ExtentTestManager.getTest().log(LogStatus.PASS, "Test passed");
+    }
+
+    @Override
+    public void onTestSuccess(ITestResult iTestResult) {
+        System.out.println("I am in onTestSuccess method " +  getTestMethodName(iTestResult) + " success");
+
+        //Get driver from BaseTest and assign to local webdriver variable.
+        Object testClass = iTestResult.getInstance();
+        WebDriver driver = ((AppTest) testClass).getDriver();
+
+        //Allure ScreenShotRobot and SaveTestLog
+        if (driver instanceof WebDriver) {
+            System.out.println("Screenshot captured for test case:" + getTestMethodName(iTestResult));
+            saveScreenshotPNG(driver);
+        }
+
+        //Save a log on allure.
+        saveTextLog(getTestMethodName(iTestResult) + " passed and screenshot taken!");
+
+        //Take base64Screenshot screenshot for extent reports
+        String base64Screenshot = "data:image/png;base64,"+((TakesScreenshot)driver).
+                getScreenshotAs(OutputType.BASE64);
+
+//        //Extentreports log and screenshot operations for failed tests.
+//        ExtentTestManager.getTest().log(LogStatus.FAIL,"Test Failed",
+//                ExtentTestManager.getTest().addBase64ScreenShot(base64Screenshot));
+        Reporter.getOutput(iTestResult);
+        logOutput(Reporter.getOutput(iTestResult));
+        
+    }
+
+    @Override
+    public void onTestSkipped(ITestResult iTestResult) {
+        System.out.println("I am in onTestSkipped method "+  getTestMethodName(iTestResult) + " skipped");
+        //Extentreports log operation for skipped tests.
+      //  ExtentTestManager.getTest().log(LogStatus.SKIP, "Test Skipped");
+    }
+
+    @Override
+    public void onTestFailedButWithinSuccessPercentage(ITestResult iTestResult) {
+        System.out.println("Test failed but it is in defined success ratio " + getTestMethodName(iTestResult));
+    }
+
+}
